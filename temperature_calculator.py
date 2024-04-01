68e4715446216d0a2807a413efd131b533895410
aawash <abhatta@stu.jsu.edu>
2024-03-31 23:46:51 -0500

    fixed typo

--- temperature_calculator.py
+++ temperature_calculator.py
@@ -1,4 +1,4 @@
-enter = input("In what measurement do you want your temperature? Input 'C' for celsius or'F' for fahrenheit: ")
+enter = input("In what measurement do you want to convert your temperature? Input 'C' for celsius or'F' for fahrenheit: ")
 if enter.lower() == "c" :
     fahrenheit = int(input("Enter the temperature in fahrenheit: "))
     celsius = (fahrenheit - 32)/1.8
