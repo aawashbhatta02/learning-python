ae192750ca99a6b7ddfe9a6fe144fd708bae7f57
aawash <abhatta@stu.jsu.edu>
2024-03-31 23:43:14 -0500

    Temperature convertor

---
+++ temperature_calculator.py
@@ -0,0 +1,11 @@
+enter = input("In what measurement do you want your temperature? Input 'C' for celsius or'F' for fahrenheit: ")
+if enter.lower() == "c" :
+    fahrenheit = int(input("Enter the temperature in fahrenheit: "))
+    celsius = (fahrenheit - 32)/1.8
+    print(celsius)
+
+else:
+    celsius = int(input("Enter the temperature in celsius: "))
+    fahrenheit = (celsius * 1.8) + 32
+    print(fahrenheit)
+
