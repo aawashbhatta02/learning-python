07adb78a77a2baf84d3e13151221068cfa3da33c
aawash <abhatta@stu.jsu.edu>
2024-03-31 23:07:22 -0500

    my initials

---
+++ my_name_initial.py
@@ -0,0 +1,7 @@
+print("     A               B B B B B                      ")
+print("    A  A             B        B                 ")
+print("   A    A            B B B B B                  ")
+print("  A A A AA           B B B B B                       ")
+print(" A        A          B        B                    ")
+print("A          A         B B B B B                       ")
+ 
